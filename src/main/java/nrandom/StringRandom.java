0914a0cff8d8f77772cb6170048519c4cb6b06ad
Nguyen Ho <Nguyen.Ho@lexisnexisrisk.com>
2017-07-05 11:44:08 +0700

    refactor code

--- src/main/java/nrandom/StringRandom.java
+++ src/main/java/nrandom/StringRandom.java
@@ -5,7 +5,7 @@ import org.apache.commons.lang3.RandomStringUtils;
 /**
  * Created by nguyenho on 7/5/2017.
  */
-public class NRandomString {
+public class StringRandom {
     public static String getWithLength(int length) {
         return RandomStringUtils.random(length);
     }
